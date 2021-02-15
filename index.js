2792a1e5d6ac2bfae1327bb9ddf3b397ecd62f68
Marcel3212 <79040095+Marcel3212@users.noreply.github.com>
2021-02-15 12:32:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6285372906349@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
 /************************************/
 
        
