0acda71430557ecd4fca45c3310f874935cd91c3
Marcel3212 <79040095+Marcel3212@users.noreply.github.com>
2021-02-15 12:29:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:Affis Admin\n' 
             + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6285372906349:+62 853-7290-6349\n' 
             + 'END:VCARD' 
 blocked = []   
 prefix = '#'
@@ -47,7 +47,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6285372906349@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
 /************************************/
 
        
